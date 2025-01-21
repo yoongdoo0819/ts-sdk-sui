373385d4165cf2c8e4f5f2353f1ee5204ae62ccf
yoongdoo0819 <yoongdoo0819@gmail.com>
2025-01-21 23:47:12 +0900

    feat: inference run

--- index.js
+++ index.js
@@ -14,17 +14,20 @@ const network = "devnet";
 const TENSROFLOW_SUI_PACKAGE_ID = '0x8872e5e8fff33fa2917f5048bbadc927336e14a778035a9baa78c9599e9ecc4b';
 const PRIVATE_KEY = "";
 
-let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 79, 44, 0, 0, 0, 0, 4, 89, 0, 0, 0, 0, 0, 59, 92, 43, 0, 0, 0, 0, 49, 89, 90, 30, 0, 0, 0, 0, 61, 81, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 79, 44, 0, 0, 0, 0, 4, 89, 0, 0, 0, 0, 0, 59, 92, 43, 0, 0, 0, 0, 49, 89, 90, 30, 0, 0, 0, 0, 61, 81, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 // let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 22, 46, 0, 0, 0, 0, 12, 8, 75, 0, 0, 0, 0, 0, 85, 30, 0, 0, 0, 0, 0, 60, 0, 0, 0, 0, 0, 0, 50, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0];
 // let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 12, 0, 0, 0, 0, 0, 93, 0, 0, 0, 0, 0, 83, 1, 0, 0, 0, 0, 28, 62, 0, 0, 0, 0, 0, 97, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 const scale = 2;
 
 const rpcUrl = getFullnodeUrl(network);
 const client = new SuiClient({ url: rpcUrl });
 
-let SignedFixedGraph = "";
-let PartialDenses = "";
+let SignedFixedGraph = ""; //"0xc01aae7a6c51c4632f292b6e6e24578f9028ba5a0a84e287b061f8aeedba443a";
+let PartialDenses = ""; //"0x72ad5e743e25ed303d4feb248798197e7b5cfe3a152da91b5d160f691c7ce138";
 
 // async function main() {
 if (!PRIVATE_KEY) {
@@ -392,6 +395,37 @@ async function waitForCommand() {
 			console.log(result);
 			break;
 	
+		case "run":
+			console.log("running...");
+			tx = new Transaction();
+
+			if (!tx.gas) {
+				console.error("Gas object is not set correctly");
+			}
+
+			tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::inference::run`,
+				arguments: [
+					tx.pure.vector('u64', input_mag),
+					tx.pure.vector('u64', input_sign),
+					tx.pure.u64(scale),
+				],
+			})
+
+			console.log("Executing transaction block..."); 
+			keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+			result = await client.signAndExecuteTransaction({
+				transaction: tx,
+				signer: keypair,
+				options: {
+					showEffects: true,
+					showEvents: true,
+					showObjectChanges: true,
+				}
+			})
+
+			console.log(result);
+			break;
 
 		default:
 			console.log(`알 수 없는 명령어: '${command}'`);
