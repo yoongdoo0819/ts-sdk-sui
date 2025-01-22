9ecd20f9e1caa3a989405efc92f274b0da52fadf
yoongdoo0819 <yoongdoo0819@gmail.com>
2025-01-23 01:33:13 +0900

    feat: add activation

--- index.js
+++ index.js
@@ -11,14 +11,27 @@ import promptSync from 'prompt-sync';
 const prompt = promptSync();
 
 const network = "devnet";
-const TENSROFLOW_SUI_PACKAGE_ID = '0x8872e5e8fff33fa2917f5048bbadc927336e14a778035a9baa78c9599e9ecc4b';
+const TENSROFLOW_SUI_PACKAGE_ID = '0xea969675a5727adf09d3a5903b52e5df5c759cd031aae4e3d35914a31c6e7257';
 const PRIVATE_KEY = "";
 
-// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 79, 44, 0, 0, 0, 0, 4, 89, 0, 0, 0, 0, 0, 59, 92, 43, 0, 0, 0, 0, 49, 89, 90, 30, 0, 0, 0, 0, 61, 81, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// 3
+// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 18, 85, 99, 17, 0, 0, 0, 3, 0, 56, 32, 0, 0, 0, 62, 93, 90, 0, 0, 0, 0, 0, 0, 99, 0, 0, 0, 90, 76, 94, 27, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 // let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 22, 46, 0, 0, 0, 0, 12, 8, 75, 0, 0, 0, 0, 0, 85, 30, 0, 0, 0, 0, 0, 60, 0, 0, 0, 0, 0, 0, 50, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0];
+
+// 7
+// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 39, 39, 39, 71, 0, 0, 0, 0, 0, 0, 54, 16, 0, 0, 3, 74, 52, 10, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 	0, 0];
+// let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 
+
+// 4
+// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 66, 0, 0, 0, 99, 0, 0, 95, 0, 0, 0, 51, 60, 87, 99, 0, 0, 0, 6, 67, 0, 99, 2, 0, 0, 0, 0, 0, 33, 57, 0, 0, 0, 0, 0, 0, 0, 0];
 // let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 45, 12, 0, 0, 0, 0, 0, 93, 0, 0, 0, 0, 0, 83, 1, 0, 0, 0, 0, 28, 62, 0, 0, 0, 0, 0, 97, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// 0
+// let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 32, 96, 1, 0, 0, 0, 33, 62, 29, 45, 0, 0, 0, 50, 0, 0, 88, 0, 0, 17, 0, 0, 54, 7, 0, 0, 11, 97, 86, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+// let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+// 6
+let input_mag = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 79, 44, 0, 0, 0, 0, 4, 89, 0, 0, 0, 0, 0, 59, 92, 43, 0, 0, 0, 0, 49, 89, 90, 30, 0, 0, 0, 0, 61, 81, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 let input_sign  = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 const scale = 2;
@@ -34,171 +47,171 @@ if (!PRIVATE_KEY) {
 	console.error("Please provide a PRIVATE_KEY in .env file");
 }
 
-async function init() {
+// async function init() {
 	
-	try {
-		console.log("Initializing...");
-		const tx = new Transaction();
+// 	try {
+// 		console.log("Initializing...");
+// 		const tx = new Transaction();
 
-		if (!tx.gas) {
-			console.error("Gas object is not set correctly");
-		}
+// 		if (!tx.gas) {
+// 			console.error("Gas object is not set correctly");
+// 		}
 
-		tx.moveCall({
-			target: `${TENSROFLOW_SUI_PACKAGE_ID}::inference_ptb::initialize`,
-		})
+// 		tx.moveCall({
+// 			target: `${TENSROFLOW_SUI_PACKAGE_ID}::inference_ptb::initialize`,
+// 		})
 
-		console.log("Executing transaction block..."); 
-		const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
-		console.log(rpcUrl)
-		console.log(client)
-		const result = await client.signAndExecuteTransaction({
-			transaction: tx,
-			signer: keypair,
-			options: {
-				showEffects: true,
-				showEvents: true,
-				showObjectChanges: true,
-			}
-		})
+// 		console.log("Executing transaction block..."); 
+// 		const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+// 		console.log(rpcUrl)
+// 		console.log(client)
+// 		const result = await client.signAndExecuteTransaction({
+// 			transaction: tx,
+// 			signer: keypair,
+// 			options: {
+// 				showEffects: true,
+// 				showEvents: true,
+// 				showObjectChanges: true,
+// 			}
+// 		})
 
-		console.log(result);
-	} catch (error) {
-		console.error("err", error)
-	}
-}
+// 		console.log(result);
+// 	} catch (error) {
+// 		console.error("err", error)
+// 	}
+// }
 
-async function compute2() {
-	const tx = new Transaction();
+// async function compute2() {
+// 	const tx = new Transaction();
 
-	if (!tx.gas) {
-		console.error("Gas object is not set correctly");
-	}
+// 	if (!tx.gas) {
+// 		console.error("Gas object is not set correctly");
+// 	}
 
-	let index = 0
-	for (let i = 0; i<2; i++) {
-		console.log("index ", index + " - " + index+1)
-		tx.moveCall({
-			target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_compute_chunk`,
-			arguments: [
-				tx.object(SignedFixedGraph),
-				tx.object(PartialDenses),
-				tx.pure.string('dense1'),
-				tx.pure.vector('u64', input_mag),
-				tx.pure.vector('u64', input_sign),
-				tx.pure.u64(index),
-				tx.pure.u64(index+1),
-			],
-		})
-		index += 2
+// 	let index = 0
+// 	for (let i = 0; i<2; i++) {
+// 		console.log("index ", index + " - " + index+1)
+// 		tx.moveCall({
+// 			target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_compute_chunk`,
+// 			arguments: [
+// 				tx.object(SignedFixedGraph),
+// 				tx.object(PartialDenses),
+// 				tx.pure.string('dense1'),
+// 				tx.pure.vector('u64', input_mag),
+// 				tx.pure.vector('u64', input_sign),
+// 				tx.pure.u64(index),
+// 				tx.pure.u64(index+1),
+// 			],
+// 		})
+// 		index += 2
 
-		console.log("Executing transaction block..."); 
-		const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
-		const result = await client.signAndExecuteTransaction({
-			transaction: tx,
-			signer: keypair,
-			options: {
-				showEffects: true,
-				showEvents: true,
-				showObjectChanges: true,
-			}
-		})
+// 		console.log("Executing transaction block..."); 
+// 		const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+// 		const result = await client.signAndExecuteTransaction({
+// 			transaction: tx,
+// 			signer: keypair,
+// 			options: {
+// 				showEffects: true,
+// 				showEvents: true,
+// 				showObjectChanges: true,
+// 			}
+// 		})
 
-		console.log(result);
-	}
-}
+// 		console.log(result);
+// 	}
+// }
 
-async function compute8() {
-	const tx = new Transaction();
+// async function compute8() {
+// 	const tx = new Transaction();
 
-	if (!tx.gas) {
-		console.error("Gas object is not set correctly");
-	}
+// 	if (!tx.gas) {
+// 		console.error("Gas object is not set correctly");
+// 	}
 
-	let index = 0
-	for (let i = 0; i<8; i++) {
-		console.log("index ", index + " - " + index+1)
-		tx.moveCall({
-			target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_compute_chunk`,
-			arguments: [
-				tx.object(SignedFixedGraph),
-				tx.object(PartialDenses),
-				tx.pure.string('dense1'),
-				tx.pure.vector('u64', input_mag),
-				tx.pure.vector('u64', input_sign),
-				tx.pure.u64(index),
-				tx.pure.u64(index+1),
-			],
-		})
-		index += 2
+// 	let index = 0
+// 	for (let i = 0; i<8; i++) {
+// 		console.log("index ", index + " - " + index+1)
+// 		tx.moveCall({
+// 			target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_compute_chunk`,
+// 			arguments: [
+// 				tx.object(SignedFixedGraph),
+// 				tx.object(PartialDenses),
+// 				tx.pure.string('dense1'),
+// 				tx.pure.vector('u64', input_mag),
+// 				tx.pure.vector('u64', input_sign),
+// 				tx.pure.u64(index),
+// 				tx.pure.u64(index+1),
+// 			],
+// 		})
+// 		index += 2
 
-		console.log("Executing transaction block..."); 
-		const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
-		const result = await client.signAndExecuteTransaction({
-			transaction: tx,
-			signer: keypair,
-			options: {
-				showEffects: true,
-				showEvents: true,
-				showObjectChanges: true,
-			}
-		})
+// 		console.log("Executing transaction block..."); 
+// 		const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+// 		const result = await client.signAndExecuteTransaction({
+// 			transaction: tx,
+// 			signer: keypair,
+// 			options: {
+// 				showEffects: true,
+// 				showEvents: true,
+// 				showObjectChanges: true,
+// 			}
+// 		})
 
-		console.log(result);
-	}
-}
+// 		console.log(result);
+// 	}
+// }
 
 
-async function finalize() {
-	const tx = new Transaction();
+// async function finalize() {
+// 	const tx = new Transaction();
 
-	if (!tx.gas) {
-		console.error("Gas object is not set correctly");
-	}
+// 	if (!tx.gas) {
+// 		console.error("Gas object is not set correctly");
+// 	}
 
-	let res1 = tx.moveCall({
-		target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_finalize`,
-		arguments: [
-			tx.object(SignedFixedGraph),
-			tx.object(PartialDenses),
-			tx.pure.string('dense1'),
-		],
-	})
+// 	let res1 = tx.moveCall({
+// 		target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_finalize`,
+// 		arguments: [
+// 			tx.object(SignedFixedGraph),
+// 			tx.object(PartialDenses),
+// 			tx.pure.string('dense1'),
+// 		],
+// 	})
 
-	let res2 = tx.moveCall({
-		target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_2`,
-		arguments: [
-			tx.object(SignedFixedGraph),
-			res1[0],
-			res1[1],
-			res1[2],
-		],
-	})
+// 	let res2 = tx.moveCall({
+// 		target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_2`,
+// 		arguments: [
+// 			tx.object(SignedFixedGraph),
+// 			res1[0],
+// 			res1[1],
+// 			res1[2],
+// 		],
+// 	})
 
-	tx.moveCall({
-		target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_3`,
-		arguments: [
-			tx.object(SignedFixedGraph),
-			res2[0],
-			res2[1],
-			res2[2],
-		],
-	})
+// 	tx.moveCall({
+// 		target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_3`,
+// 		arguments: [
+// 			tx.object(SignedFixedGraph),
+// 			res2[0],
+// 			res2[1],
+// 			res2[2],
+// 		],
+// 	})
 
-	console.log("Executing transaction block..."); 
-	const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
-	const result = await client.signAndExecuteTransaction({
-		transaction: tx,
-		signer: keypair,
-		options: {
-			showEffects: true,
-			showEvents: true,
-			showObjectChanges: true,
-		}
-	})
+// 	console.log("Executing transaction block..."); 
+// 	const keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+// 	const result = await client.signAndExecuteTransaction({
+// 		transaction: tx,
+// 		signer: keypair,
+// 		options: {
+// 			showEffects: true,
+// 			showEvents: true,
+// 			showObjectChanges: true,
+// 		}
+// 	})
 
-	console.log(result);
-}
+// 	console.log(result);
+// }
 
 async function waitForCommand() {
 	// 사용자 입력 받기
@@ -427,6 +440,197 @@ async function waitForCommand() {
 			console.log(result);
 			break;
 
+		case "compute_2":
+			// compute2();
+			
+			index = 0;
+			for (let i = 0; i<2; i++) {
+				console.log(index, index+7)
+				tx = new Transaction();
+
+				if (!tx.gas) {
+					console.error("Gas object is not set correctly");
+				}
+				
+				tx.moveCall({
+					target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_chunk`,
+					arguments: [
+						tx.object(SignedFixedGraph),
+						tx.object(PartialDenses),
+						tx.pure.string('dense1'),
+						tx.pure.vector('u64', input_mag),
+						tx.pure.vector('u64', input_sign),
+						tx.pure.u64(1),
+						tx.pure.u64(index),
+						tx.pure.u64(index+7),
+					],
+				})
+				index += 8
+
+				console.log("Executing transaction block..."); 
+				keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+				result = await client.signAndExecuteTransaction({
+					transaction: tx,
+					signer: keypair,
+					options: {
+						showEffects: true,
+						showEvents: true,
+						showObjectChanges: true,
+					}
+				})
+
+				console.log(result);
+			}
+			break;
+			
+		case "compute_8":
+			// compute8();
+
+			index = 0;
+			for (let i = 0; i<8; i++) {
+				console.log(index, index+1)
+				tx = new Transaction();
+
+				if (!tx.gas) {
+					console.error("Gas object is not set correctly");
+				}
+				
+				tx.moveCall({
+					target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_chunk`,
+					arguments: [
+						tx.object(SignedFixedGraph),
+						tx.object(PartialDenses),
+						tx.pure.string('dense1'),
+						tx.pure.vector('u64', input_mag),
+						tx.pure.vector('u64', input_sign),
+						tx.pure.u64(1),
+						tx.pure.u64(index),
+						tx.pure.u64(index+1),
+					],
+				})
+				index += 2
+
+				console.log("Executing transaction block..."); 
+				keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+				result = await client.signAndExecuteTransaction({
+					transaction: tx,
+					signer: keypair,
+					options: {
+						showEffects: true,
+						showEvents: true,
+						showObjectChanges: true,
+					}
+				})
+
+				console.log(result);
+			}
+			break;
+
+		case "finalize_activation":
+			// finalize();
+			tx = new Transaction();
+
+			if (!tx.gas) {
+				console.error("Gas object is not set correctly");
+			}
+
+			let res_act1 = tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_finalize`,
+				arguments: [
+					// tx.object(SignedFixedGraph),
+					tx.object(PartialDenses),
+					tx.pure.string('dense1'),
+				],
+			})
+
+			let res_act2 = tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_2`,
+				arguments: [
+					tx.object(SignedFixedGraph),
+					res_act1[0],
+					res_act1[1],
+					res_act1[2],
+				],
+			})
+
+			tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_3`,
+				arguments: [
+					tx.object(SignedFixedGraph),
+					res_act2[0],
+					res_act2[1],
+					res_act2[2],
+				],
+			})
+
+			console.log("Executing transaction block..."); 
+			keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+			result = await client.signAndExecuteTransaction({
+				transaction: tx,
+				signer: keypair,
+				options: {
+					showEffects: true,
+					showEvents: true,
+					showObjectChanges: true,
+				}
+			})
+
+			console.log(result);
+			break;
+		
+		case "allptb":
+			// finalize();
+			tx = new Transaction();
+
+			if (!tx.gas) {
+				console.error("Gas object is not set correctly");
+			}
+
+			let ptb_res1 = tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_1`,
+				arguments: [
+					tx.object(SignedFixedGraph),
+					tx.pure.vector('u64', input_mag),
+					tx.pure.vector('u64', input_sign),
+					tx.pure.u64(scale),
+				],
+			})
+
+			let ptb_res2 = tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_2`,
+				arguments: [
+					tx.object(SignedFixedGraph),
+					ptb_res1[0],
+					ptb_res1[1],
+					ptb_res1[2],
+				],
+			})
+
+			tx.moveCall({
+				target: `${TENSROFLOW_SUI_PACKAGE_ID}::model_ptb::ptb_graph_3`,
+				arguments: [
+					tx.object(SignedFixedGraph),
+					ptb_res2[0],
+					ptb_res2[1],
+					ptb_res2[2],
+				],
+			})
+
+			console.log("Executing transaction block..."); 
+			keypair = Ed25519Keypair.fromSecretKey(fromHex(PRIVATE_KEY));
+			result = await client.signAndExecuteTransaction({
+				transaction: tx,
+				signer: keypair,
+				options: {
+					showEffects: true,
+					showEvents: true,
+					showObjectChanges: true,
+				}
+			})
+
+			console.log(result);
+			break;
+			
 		default:
 			console.log(`알 수 없는 명령어: '${command}'`);
 		}
